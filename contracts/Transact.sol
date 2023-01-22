e873471648c12091ea101058ecb9cef7278d3afe
Ayush Patel <ayush200423@gmail.com>
2023-01-22 07:36:44 -0800

    better format

--- contracts/Transact.sol
+++ contracts/Transact.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Transact {
-
+contract ChainLink {
     struct TransferStruct {
-        uint hash;
         string name;
         string location;
         string date;
@@ -12,17 +10,24 @@ contract Transact {
         address sender;
         address recipient;
     }
+    
+    TransferStruct[] public arr;
+    mapping(uint => TransferStruct[]) public lookup;
 
-    TransferStruct[] transactions;
+    //TransferStruct[] transactions;
 
     function sendPayment(uint _hash, string memory _name, string memory _location, string memory _date, string memory _details, address payable _recipient) payable public {
         (bool sent, bytes memory data) = _recipient.call{value: msg.value}("");
         require(sent, "Failed to send Ether");
         // update the values
-        transactions.push(TransferStruct(_hash, _name, _location, _date, _details, msg.sender, _recipient)); 
+        lookup[_hash].push(TransferStruct(_name, _location, _date, _details, msg.sender, _recipient));
     }
 
-    function getAllPayments() public view returns (TransferStruct[] memory) {
-        return transactions;
+    function createNewHash(uint _hash) public {
+        lookup[_hash] = arr;
+    }
+
+    function getAllPayments(uint _hash) public view returns (TransferStruct[] memory) {
+        return lookup[_hash];
     }
 }
