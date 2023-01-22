8ac11d7e06a6baed5d5f4a44e86d367d84b80d7d
John Borillo <73507954+johnborillo@users.noreply.github.com>
2023-01-22 09:51:47 -0800

    Added new HashToQR file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { Component } from 'react';  
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';  
-import Home from './Components/home';  
-import Scanner from './Components/scanner';  
-import './App.css';  
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Components/home';
+import Scanner from './Components/scanner';
+import './App.css';
 import NewHash from './Components/newhash';
-   
-class App extends Component {  
-  render() {  
-    return (  
-       <Router>  
-           <div className="App">  
-            <ul className="App-header">  
-              <li>  
-                <Link to="/">Home</Link>  
-              </li>  
-              <li>  
-                <Link to="/newhash">New Hash</Link>  
-              </li>  
-              <li>  
-                <Link to="/scanner">Scanner</Link>  
-              </li>  
-            </ul>  
-           <Routes>  
-                 <Route exact path='/' element={< Home />}></Route>  
-                 <Route exact path='/newhash' element={< NewHash />}></Route>  
-                 <Route exact path='/scanner' element={< Scanner />}></Route>  
-          </Routes>  
-          </div>  
-       </Router>  
-   );  
-  }  
-}  
+import HashToQR from './Components/hashtoqr';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <ul className="App-header">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/newhash">New Hash</Link>
+            </li>
+            <li>
+              <Link to="/scanner">Scanner</Link>
+            </li>
+            <li>
+              <Link to="/hashtoqr">Hash to QR</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/newhash' element={< NewHash />}></Route>
+            <Route exact path='/scanner' element={< Scanner />}></Route>
+            <Route exact path='/hashtoqr' element={< HashToQR />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+}
 export default App;  
