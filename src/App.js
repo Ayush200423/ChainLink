cac37f40641069973cc12d4e1a96b3012b6edfd1
John Borillo <73507954+johnborillo@users.noreply.github.com>
2023-01-21 18:22:22 -0800

    Added content to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { } from 'react';
 import QRScanner from './Components/QRSacnner';
+import './App.css'
 
 function App() {
   return (
     <div>
-      <h1>Welcome to the landing page</h1>
-      <QRScanner />
+      <header>
+        <div className="nav" >
+          <p className="logo">LOGO</p>
+          <ul>
+            <li><a href>HOME</a></li>
+            <li><a href>SCANNER</a></li>
+          </ul>
+        </div>
+        <div class="welcome">
+          <div class="leftSide">
+            <h1>Welcome to the landing page</h1>
+            <QRScanner />            
+            </div>
+        </div>
+      </header>
     </div>
+
   );
 
-  
+
 }
 
 
