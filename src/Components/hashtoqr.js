cfac294628818e339acee7c20252065c4410c58a
anica <anicamok@gmail.com>
2023-01-22 10:52:29 -0800

    change in background picture

--- src/Components/hashtoqr.js
+++ src/Components/hashtoqr.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import QRCode from 'qrcode.react';
+import './hashtoqr.css';
 
 const HashToQR = () => {
   const [hash, setHash] = useState('');
 
   return (
     <div>
-      <input 
-        type="text" 
-        placeholder="Enter Hash" 
-        onChange={e => setHash(e.target.value)}
-        value={hash}
-      />
-      <QRCode value={hash} />
+      <div class="textBox">
+        <input
+          type="text"
+          placeholder="Enter Hash"
+          onChange={e => setHash(e.target.value)}
+          value={hash}
+        />
+      </div>
+      <div class="qr">
+        <QRCode value={hash} />
+      </div>
     </div>
   );
 };
