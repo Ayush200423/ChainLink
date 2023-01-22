e42fc1f634073c1460dec7530bcd06246c4df0bb
John Borillo <73507954+johnborillo@users.noreply.github.com>
2023-01-22 11:02:39 -0800

    added "newhash" feature

--- src/Components/newhash.js
+++ src/Components/newhash.js
@@ -1,13 +1,49 @@
-import React from 'react';  
-import './newhash.css';
+import React, { useState } from 'react';
+import CryptoJS from 'crypto-js';
 
-function NewHash () {  
-    return <div>  
-        <h2> jAVAtPOINT is a computer science portal for TUTORIALS!</h2>  
-   Read more about us at :  
-        <a href="https://www.javatpoint.in/about/">  
-            https://www.javatpoint.in/about/  
-        </a>  
-    </div>  
-}  
-export default NewHash;  
+const NewHash = () => {
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [date, setDate] = useState('');
+  const [details, setDetails] = useState('');
+  const [hash, setHash] = useState('');
+
+  const handleGenerate = () => {
+    const data = `${name}-${location}-${date}-${details}`;
+    const hash = CryptoJS.SHA256(data).toString(CryptoJS.enc.Hex);
+    setHash(hash);
+  };
+
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Name" 
+        onChange={e => setName(e.target.value)}
+        value={name}
+      />
+      <input 
+        type="text" 
+        placeholder="Location" 
+        onChange={e => setLocation(e.target.value)}
+        value={location}
+      />
+      <input 
+        type="text" 
+        placeholder="Date" 
+        onChange={e => setDate(e.target.value)}
+        value={date}
+      />
+      <input 
+        type="text" 
+        placeholder="Details" 
+        onChange={e => setDetails(e.target.value)}
+        value={details}
+      />
+      <button onClick={handleGenerate}>Generate</button>
+      <p>Hash: {hash}</p>
+    </div>
+  );
+};
+
+export default NewHash;
